3ef027f04f250448cc3d7efc06c0a03331c85422
VictorGus <retaow@gmail.com>
2022-01-23 23:49:18 +0300

    progress

--- src/app/html.clj
+++ src/app/html.clj
@@ -47,6 +47,12 @@
                      :padding-left  ".6em"
                      :border-radius "10rem"}]
    [:.exp-connector {:border-right "1px solid #ff0a0a!important"}]
+   [:.about-text    {:font-size "20px"}]
+   [:.card          {:padding "1.25rem"}]
+   [:.card-text     {:padding "1rem 0"}]
+   [:.card-link     {:color "#fc6f5c"
+                     :text-decoration "none"
+                     :font-size "20px"}]
    [".exp-divider::after"   {:content "'\\00B7'"}]
    [".skills-header::before" {:content "''"
                               :position "absolute"
@@ -87,8 +93,8 @@
      [:div.col-10.mx-auto.col-md-6.order-md-2
       [:img.img-fluid.photo {:src photo}]]
      [:div.col-md-6.order-md-1.text-center.text-md-left.align-self-center
-      [:h2.iam-header (format "I am %s" name)]
-      [:p text]]]]
+      [:h2.iam-header "I am " [:strong name]]
+      [:p.about-text text]]]]
    [:section.inner-area.grey-area
     [:div.container
      [:div.row
@@ -155,6 +161,22 @@
            ]]]))
     experience)])
 
+(defn accomplishments-section [{:keys [accomplishments] :as config}]
+  [:div.row
+   (for [{:keys [title subtitle link date text]} accomplishments]
+     [:div.col-lg-6.col-md-6.mb-4
+      [:div.card.rounded-0.border-0.w-shadow
+       [:div.card-body
+        [:h4.main-title.card-title.text-muted.my-0 title]
+        [:div.text-muted.exp-meta
+         subtitle
+         [:span.exp-divider]
+         [:span.exp-location date]]
+        [:div.card-text
+         text]
+        [:a.card-link {:target "_blank" :href link}
+         "See certificate"]]]])])
+
 (defn cv-as-hiccup [config]
   [:html
    [:head
@@ -178,6 +200,10 @@
          [:a.nav-link {:href "#about"
                        :data-target "#about"}
           [:span "Home"]]]
+        [:li.nav-item
+         [:a.nav-link {:data-target "#accomplishments"
+                       :href "#accomplishments"}
+          [:span "Accomplishments"]]]
         [:li.nav-item
          [:a.nav-link
           [:span "Publications"]]]
@@ -208,8 +234,13 @@
         [:h1.skills-header
          "Experience"]]
        (experience-section config)]]]
-    ]]
-  )
+    [:section.about-section {:id "accomplishments"}
+     [:div.container
+      [:div.text-center.pb-5
+       [:h1.skills-header "Accomplishments"]]
+      (accomplishments-section config)
+      ]]
+    ]])
 
 (spit "/home/victor/Documents/Pet-Projects/cv/resources/index.html" (hc/html (cv-as-hiccup {:about  {:photo "https://thomasgeorgethomas.com/img/Profile_Picture.jpg"
                                                                                                      :text "A Data Engineer passionate about Data Science 📊. I like automating things, building pipelines, exploring scalability problems, improving efficiency and performance tuning. I’m a strong advocate for 📜 open source, ☁️ Cloud computing, 🚀 DevOps, 🆕 Innovation and Automation"
@@ -222,6 +253,16 @@
                                                                                                                   :location  ""
                                                                                                                   :graduation "2021"}]
                                                                                                      :interests ["foo bar"]}
+                                                                                            :accomplishments [{:title "Foo bar"
+                                                                                                               :subtitle "Foo"
+                                                                                                               :date "Feb 21"
+                                                                                                               :link "http://localhost:3333"
+                                                                                                               :text "Foo bar barr get test brr"}
+                                                                                                              {:title "Foo bar"
+                                                                                                               :subtitle "Foo"
+                                                                                                               :date "Feb 21"
+                                                                                                               :link "http://localhost:3333"
+                                                                                                               :text "Foo bar barr get test brr"}]
                                                                                             :experience [{:company {:name "Blabla"
                                                                                                                     :location "Russia, St. Petersburg"}
                                                                                                           :position "Software Engineer"
@@ -264,231 +305,3 @@
                                                                                                       :photo "./public/logos/linux-icon.svg"}
                                                                                                      {:name "Vim"
                                                                                                       :photo "./public/logos/vim-icon.svg"}]})))
-
-#_(spit
- "/home/victor/Documents/Pet-Projects/cv/resources/index.html"
- (hc/html
-  [:html
-   [:head
-    [:link {:href       "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-            :rel        "stylesheet"
-            :integrity  "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-            :crossorigin "anonymous"}]
-    [:link {:href        "https://use.fontawesome.com/releases/v5.6.0/css/all.css"
-            :rel         "stylesheet"
-            :integrity   "sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h"
-            :crossorigin "anonymous"}]]
-   [:body
-    [:style (stringify-styles styles)]
-    [:nav.navbar.navbar-light.fixed-top.navbar-expand-lg.navbar-shadow.bg-white
-     [:div.container.d-flex
-      [:div.mr-auto.p-2
-       [:a.navbar-brand "Viktor Gusakov"]]
-      [:div.p-2
-       [:ul.navbar-nav.ml-auto
-        [:li.nav-item
-         [:a.nav-link {:href "#about"
-                       :data-target "#about"}
-          [:span "Home"]]]
-        [:li.nav-item
-         [:a.nav-link
-          [:span "Publications"]]]
-        [:li.nav-item
-         [:a.nav-link {:href "#skills"
-                       :data-target "#skills"}
-          [:span "Skills"]]]
-        [:li.nav-item
-         [:a.nav-link {:href "#experience"
-                       :data-target "#experience"}
-          [:span "Experience"]]]
-        [:li.nav-item
-         [:a.nav-link
-          [:span "Projects"]]]
-        [:li.nav-item
-         [:a.nav-link
-          [:span "Contact"]]]
-        [:li.nav-item
-         [:a.nav-link
-          [:span "Resume"]]]]]]]
-    [:section.about-section.pb-0 {:id "about"}
-     [:div.container.pb-5
-      [:div.row
-       [:div.col-10.mx-auto.col-md-6.order-md-2
-        [:img.img-fluid.photo {:src "https://thomasgeorgethomas.com/img/Profile_Picture.jpg"}]]
-       [:div.col-md-6.order-md-1.text-center.text-md-left.align-self-center
-        [:h2.iam-header "I am Viktor Gusakov"]
-        [:p "A Data Engineer passionate about Data Science 📊. I like automating things, building pipelines, exploring scalability problems, improving efficiency and performance tuning. I’m a strong advocate for 📜 open source, ☁️ Cloud computing, 🚀 DevOps, 🆕 Innovation and Automation"]]]]
-     [:section.inner-area.grey-area
-      [:div.container
-       [:div.row
-        [:div.col-md-7
-         [:h3 "Education"]
-         [:ul.ul-edu.fa-ul
-          [:li.about-el
-           [:i.fa-li.fas.fa-graduation-cap]
-           [:div.description
-            [:p.faculty
-             "M.S. Data Analytics Engineering, 2023"]
-            [:p.text-muted
-             "Northeastern University"]]]
-          [:li.about-el
-           [:i.fa-li.fas.fa-graduation-cap]
-           [:div.description
-            [:p.faculty
-             "M.S. Data Analytics Engineering, 2023"]
-            [:p.text-muted
-             "Northeastern University"]]]]]
-        [:div.col-md-5
-         [:h3 "Interests"]
-         [:ul
-          [:li.about-el "Big Data Analytics"]
-          [:li.about-el "Big Data Analytics"]
-          [:li.about-el "Big Data Analytics"]
-          [:li.about-el "Big Data Analytics"]
-          [:li.about-el "Big Data Analytics"]]]]]]]
-    [:section.about-section {:id "skills"}
-     [:div.container
-      [:div.row.pb-5
-       [:div.col-xs-1 {:align "center"}
-        [:h1.skills-header "Skills"]]]
-      [:div.row
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/golang-icon.svg"}]
-         [:h3 "Go"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/clojure-icon.svg"}]
-         [:h3 "Clojure"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/c-seeklogo.com.svg"
-                  :style "width: 64px;"}]
-         [:h3 "C/C++"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/postgresql-icon.svg"}]
-         [:h3 "PostgreSQL"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/docker-icon.svg"}]
-         [:h3 "Docker"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/kubernetes-icon.svg"}]
-         [:h3 "Kubernetes"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/elastic-icon.svg"}]
-         [:h3 "Elasticsearch"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/amazon_aws-icon.svg"}]
-         [:h3 "AWS"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/travis-ci-icon.svg"}]
-         [:h3 "Travis CI"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/git-scm-icon.svg"}]
-         [:h3 "Git"]]]
-       [:div.col-lg-3.col-sm-6.mb-4
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/linux-icon.svg"}]
-         [:h3 "Linux"]]]
-       [:div.col-lg-3.col-sm-
-        [:div.position-relative.py-2.w-shadow.text-center
-         [:embed {:src "/home/victor/Documents/Pet-Projects/cv/resources/public/logos/vim-icon.svg"}]
-         [:h3 "Vim"]]]
-       ]]]
-    [:section.about-section.grey-area {:id "experience"}
-     [:div.container
-      [:div.row.pb-4
-       [:div.col-lg-4
-        [:h1.skills-header
-         "Experience"]]
-       [:div.col-lg-8
-        [:div.row
-         [:div.col-auto.text-center.flex-column.d-none.d-sm-flex
-          [:div.row.h-50
-           [:div.col]
-           [:div.col]]
-          [:div.m-2
-           [:span.exp-badge.exp-border.badge
-            "''"]]
-          [:div.row.h-50
-           [:div.col.exp-connector]
-           [:div.col]]]
-         [:div.col.py-2
-          [:div.card.w-shadow
-           [:div.card-body
-            [:h4.main-title.card-title.text-muted.mt-0.mb-2 "Blablabla"]
-            [:h4.card-title.text-muted.my-0 "Blablabla"]
-            [:div.text-muted.exp-meta
-             "Oct 2020 – Aug 2021"
-             [:span.exp-divider]
-             [:span.exp-location "St. Petersburg, Russia"]]
-            [:div.card-text.pt-3
-             [:p.highlight "Highlights: "]
-             [:ul
-              [:li "Built data pipelines in AWS leveraging services S3, RDS, Athena, Step functions, and EMR."]
-              [:li "Migrated 112 TB of data from the on-premises Hadoop cluster to AWS and Snowflake."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]]
-             [:p "Technologies: Spark, Scala, Hive, Hadoop, Snowflake, Git, Bitbucket, Maven"]
-             ]]]
-          ]]
-        [:div.row
-         [:div.col-auto.text-center.flex-column.d-none.d-sm-flex
-          [:div.row.h-50
-           [:div.col..exp-connector]
-           [:div.col]]
-          [:div.m-2
-           [:span.exp-badge.exp-border.badge
-            "''"]]]
-         [:div.col.py-2
-          [:div.card.w-shadow
-           [:div.card-body
-            [:h4.main-title.card-title.text-muted.mt-0.mb-2 "Blablabla"]
-            [:h4.card-title.text-muted.my-0 "Blablabla"]
-            [:div.text-muted.exp-meta
-             "Oct 2020 – Aug 2021"
-             [:span.exp-divider]
-             [:span.exp-location "St. Petersburg, Russia"]]
-            [:div.card-text.pt-3
-             [:p.highlight "Highlights: "]
-             [:ul
-              [:li "Built data pipelines in AWS leveraging services S3, RDS, Athena, Step functions, and EMR."]
-              [:li "Migrated 112 TB of data from the on-premises Hadoop cluster to AWS and Snowflake."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]
-              [:li "Innovated and automated post-migration validation reports in Spark Scala bringing down costs by 90% for 2 projects."]]
-             [:p "Technologies: Spark, Scala, Hive, Hadoop, Snowflake, Git, Bitbucket, Maven"]
-             ]]]
-          ]]
-        ]]]]
-    ]]))
-
-(comment
-
-  {:about        {:photo ""
-                  :name  ""
-                  :text  ""
-                  :education [{} {}]
-                  :interests []}
-   :experience   [{:companyName ""
-                   :companyLocation ""
-                   :period ""
-                   :highlights [""]
-                   :technologies [""]}]
-   :projects     [{} {}]
-   :publications [{}]
-   :contact      {}}
- 
-
-
-  )
