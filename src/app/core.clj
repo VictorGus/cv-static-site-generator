bcf8baf0a3e4b1f518b3818bc9af43d776441d21
VictorGus <retaow@gmail.com>
2022-02-06 01:08:18 +0300

    process static

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,24 +1,26 @@
 (ns app.core
   (:require [clojure.string :as str]
-            [cli-matic.core :as cli])
+            [clojure.edn    :as edn]
+            [cli-matic.core :as cli]
+            [app.static     :as static])
   (:gen-class))
 
+
+(defn process-config [{:keys [config output] :as arg}]
+  (let [parsed-config (cond-> config ;; add new options later
+                        (str/includes? config ".edn")
+                        (-> slurp edn/read-string))]
+    (->> parsed-config (merge {:output-path output}) static/spit-cv-site)))
 
 (def configuration
   {:app         {:command     "cv-static"
                  :description "CV static site generator"
                  :version     "0.0.1"}
-   :commands    [{:command     "rest"
-                  :description "upload scraped data by url"
-                  :opts [{:option "url" :short "u" :as "url of a service" :type :string}
-                         {:option "token" :short "t" :as "bearer token"  :type :string}
-                         {:option "endpoint" :short "e" :as "bulk upload endpoint" :type :string}
-                         {:option "datamap" :short "f" :as "yaml file with urls" :type :yamlfile}]
-                  :runs  (fn [])}
-                 {:command     "straight"
+   :commands    [{:command     "gen-static"
                   :description "upload scraped data via straight connection to db"
-                  :opts [{:option "datamap" :short "f" :as "yaml file with urls" :type :yamlfile}]
-                  :runs  (fn [])}]})
+                  :opts [{:option "config" :short "c" :as "Path to configuration file" :type :string}
+                         {:option "output" :short "o" :as "Path for static files"      :type :string}]
+                  :runs process-config}]})
 
 (defn -main [& args]
   (cli/run-cmd args configuration))
--- src/app/static.clj
+++ src/app/static.clj
@@ -1,5 +1,6 @@
 (ns app.static
   (:require [hiccup.core :as hc]
+            [app.html    :as cv]
             [org.httpkit.sni-client :as sni-client]
             [org.httpkit.client :as http]
             [clojure.java.io :as io]
@@ -18,7 +19,7 @@
       (io/copy in out))
     relative-path))
 
-(defn pull-remote-resources [path config]
+(defn pull-remote-resources [{:keys [output-path] :as config}]
   (walk/postwalk
    (fn [el]
      (if (and (string? el)
@@ -28,6 +29,118 @@
                 (str/includes? el ".png")
                 (str/includes? el ".jpg")
                 (str/includes? el ".jpeg"))))
-       (save-remote-image path el)
+       (save-remote-image output-path el)
        el))
    config))
+
+(defn spit-cv-site [{:keys [output-path] :as config}]
+  (let [config* (pull-remote-resources config)]
+    (spit
+     (str output-path "/index.html")
+     (hc/html (cv/cv-as-hiccup config*)))))
+
+(comment
+
+  (spit-cv-site {:output-path "/home/victor/Documents/Pet-Projects/cv/resources"
+                 :about  {:photo "https://thomasgeorgethomas.com/img/Profile_Picture.jpg"
+                          :text "A Data Engineer passionate about Data Science 📊. I like automating things, building pipelines, exploring scalability problems, improving efficiency and performance tuning. I’m a strong advocate for 📜 open source, ☁️ Cloud computing, 🚀 DevOps, 🆕 Innovation and Automation"
+                          :name  "Viktor Gusakov"
+                          :education [{:university "Leti"
+                                       :faculty    "Foo bar"
+                                       :graduation "2019"}
+                                      {:university "Leti"
+                                       :faculty    "Foo bar"
+                                       :location  ""
+                                       :graduation "2021"}]
+                          :interests ["foo bar"]
+                          :contacts [{:name "GitHub" :type "github" :url "https://github.com/VictorGus"}
+                                     {:name "retaow@gmail.com" :type "e-mail"}
+                                     {:name "VictorGus" :type "telegram"}
+                                     {:name "Habr Career" :type "habr" :url "https://career.habr.com/victor-gusakov"}
+                                     {:name "Medium" :type "medium" :url "https://medium.com/@VictorGus"}]}
+                 :publications [{:description "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum"
+                                 :name "Foo bar"
+                                 :url "https://google.com"
+                                 :photo "https://www.dankultura.org/wp-content/uploads/2015/10/Article-Writing-Can-be-a-Sure-Wager.jpg"}
+                                {:description "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum"
+                                 :name "Foo bar"
+                                 :url "https://google.com"
+                                 :photo "https://www.dankultura.org/wp-content/uploads/2015/10/Article-Writing-Can-be-a-Sure-Wager.jpg"}
+                                {:description "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum"
+                                 :name "Foo bar"
+                                 :url "https://google.com"
+                                 :photo "https://www.dankultura.org/wp-content/uploads/2015/10/Article-Writing-Can-be-a-Sure-Wager.jpg"}]
+                 :projects [
+                            {:description "Taking a look at data of 1.6 million twitter users and drawing useful insights while exploring interesting patterns. The techniques used include text mining, sentimental analysis, probability, time series analysis and Hierarchical clustering on text/words using R"
+                             :name "Test foo"
+                             :url "https://google.com"
+                             :photo "https://vivaldi.com/wp-content/uploads/The_Pomodoro_timer_in_Vivaldi_browser-980x551.png"}
+                            {:description "Taking a look at data of 1.6 million twitter users and drawing useful insights whil"
+                             :name "Test foo"
+                             :url "https://google.com"
+                             :photo "https://vivaldi.com/wp-content/uploads/The_Pomodoro_timer_in_Vivaldi_browser-980x551.png"}
+                            {:description "Taking a look at data of 1.6 million twitter users and drawing useful insights while exploring interesting patterns. The techniques used include text mining, sentimental analysis, probability, time series analysis and Hierarchical clustering on text/words using R"
+                             :name "Test foo"
+                             :url "https://google.com"
+                             :photo "https://vivaldi.com/wp-content/uploads/The_Pomodoro_timer_in_Vivaldi_browser-980x551.png"}
+
+                            {:description "Taking a look at data of 1.6 million twitter users and drawing useful insights while"
+                             :name "Test foo foo"
+                             :url "https://google.com"
+                             :photo "https://vivaldi.com/wp-content/uploads/The_Pomodoro_timer_in_Vivaldi_browser-980x551.png"}]
+                 :accomplishments [{:title "Foo bar"
+                                    :subtitle "Foo"
+                                    :date "Feb 21"
+                                    :link "http://localhost:3333"
+                                    :text "Foo bar barr get test brr"}
+                                   {:title "Foo bar"
+                                    :subtitle "Foo"
+                                    :date "Feb 21"
+                                    :link "http://localhost:3333"
+                                    :text "Foo bar barr get test brr"}]
+                 :experience [{:company {:name "Blabla"
+                                         :location "Russia, St. Petersburg"}
+                               :position "Software Engineer"
+                               :period "Aug 2019 - Now"
+                               :highlights ["Fooo"]
+                               :technologies ["AWS"]}
+                              {:company {:name "Blabla"
+                                         :location "Russia, St. Petersburg"}
+                               :position "Software Engineer"
+                               :period "Aug 2019 - Now"
+                               :highlights ["Fooo"]
+                               :technologies ["AWS"]}
+                              {:company {:name "Blabla"
+                                         :location "Russia, St. Petersburg"}
+                               :position "Software Engineer"
+                               :period "Aug 2019 - Now"
+                               :highlights ["Fooo"]
+                               :technologies ["AWS"]}]
+                 :skills [{:name "Go"
+                           :photo "./public/logos/golang-icon.svg"}
+                          {:name "Clojure"
+                           :photo "./public/logos/clojure-icon.svg"}
+                          {:name "C/C++"
+                           :photo "./public/logos/c-seeklogo.com.svg"}
+                          {:name "PostgreSQL"
+                           :photo "./public/logos/postgresql-icon.svg"}
+                          {:name "Docker"
+                           :photo "./public/logos/docker-icon.svg"}
+                          {:name "Kubernetes"
+                           :photo "./public/logos/kubernetes-icon.svg"}
+                          {:name "Elasticsearch"
+                           :photo "./public/logos/elastic-icon.svg"}
+                          {:name "AWS"
+                           :photo "./public/logos/amazon_aws-icon.svg"}
+                          {:name "Travis CI"
+                           :photo "./public/logos/travis-ci-icon.svg"}
+                          {:name "Git"
+                           :photo "./public/logos/git-scm-icon.svg"}
+                          {:name "Linux"
+                           :photo "./public/logos/linux-icon.svg"}
+                          {:name "Vim"
+                           :photo "./public/logos/vim-icon.svg"}]})
+
+
+
+  )
