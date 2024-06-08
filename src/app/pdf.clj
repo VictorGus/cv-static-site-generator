8d4be3cf428a21b9cf63386ac49917ef225fe53d
VictorGus <retaow@gmail.com>
2024-06-09 01:25:50 +0300

    gen pdf (wip)

--- src/app/pdf.clj
+++ src/app/pdf.clj
@@ -2,18 +2,77 @@
   (:require [clojure.string :as str]
             [clj-pdf.core :as pdf]))
 
+(def titles
+  {:skills          {:russian "Навыки"
+                      :english "Skills"}
+   :education       {:russian "Образование"
+                     :english "Educaition"}
+   :iam             {:russian "Меня зовут "
+                     :english "I am "}
+   :home            {:russian "О себе"
+                     :english "Home"}
+   :publications    {:english "Publications"
+                     :russian "Публикации"}
+   :highlights      {:russian "Обязанности"
+                     :english "Highlights"}
+   :technologies    {:russian "Технологии"
+                     :english "Technologies"}
+   :projects        {:russian "Проекты"
+                     :english "Projects"}
+   :experience      {:russian "Опыт работы"
+                     :english "Experience"}
+   }
+  )
 
-(comment 
+(def styles {})
+
+(defn description-section [{{:keys [text name contacts interests]} :about :as config}]
+  [:paragraph
+   [:chunk name]]
+  )
+
+(defn skills-section [config]
+  )
+
+(defn experience-section [config]
+  )
+
+(defn pet-projects-section [config]
+  )
+
+(defn education-section [config]
+  )
+
+(defn save-cv-document [config path]
   (pdf/pdf
    [{}
-    [:list {:roman true}
-     [:chunk {:style :bold} "a bold item"]
-     "another item"
-     "yet another item"]
-    [:phrase "some text"]
-    [:phrase "some more text"]
-    [:paragraph "yet more text"]]
-   "doc.pdf")
+    (description-section config)
+    ]
+   path)
+  )
+
+(comment
+
+  (save-cv-document {:about {:text "fpokfo xoxo"}} "SAMPLE.pdf")
+
+  (pdf/pdf
+   [{}
+    [:chapter [:paragraph {:color [250 0 0]} "Chapter"]
+     [:section "Section Title" "Some content"]
+     [:section [:paragraph {:size 10} "Section Title"]
+      [:paragraph "Some content"]
+      [:paragraph "Some more content"]
+      [:section {:color [100 200 50]} [:paragraph "Nested Section Title"]
+       [:paragraph "nested section content"]]]]]
+   "SAMPLE.pdf")
+
+
+  (pdf/pdf
+   [{}
+    [:chunk {:size 20} "Viktor Gusakov"]
+    []
+    ]
+   "SAMPLE.pdf")
 
 
   )
